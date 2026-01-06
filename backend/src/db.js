3789ea8bd9d09674edb8025829528cce07dab7ed
dagmawi getaneh <dagi.getaneh3@gamil.com>
2026-01-07 01:45:33 +0300

    reattempt 3

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 
+const useSsl = (process.env.DATABASE_SSL || '').toLowerCase() === 'true';
+
 const pool = mysql.createPool({
   host: process.env.DATABASE_HOST || 'db',
   port: process.env.DATABASE_PORT ? Number(process.env.DATABASE_PORT) : 3306,
@@ -9,7 +11,8 @@ const pool = mysql.createPool({
   database: process.env.DATABASE_NAME || 'admas_dev',
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: useSsl ? { rejectUnauthorized: false } : undefined
 });
 
 module.exports = pool;
